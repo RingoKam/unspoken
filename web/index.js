1a6bba48067e00bd8587c7b86661ffdee0cdc878
ringo <ringo_kam@hotmail.com>
2023-11-26 02:19:51 -0500

    flow thru question one by one

--- web/index.js
+++ web/index.js
@@ -49,14 +49,16 @@ const defaultColor = new Color(0x000000)
 let anchorModel, anchorText;
 let text1, group1;
 
+let questions = [];
+let correctAnswer = null;
 let userCorrectAnswerInterval = 0;
 const correctIntervalThreshold = 3000;
 const incorrectAnswerMultipler = 1.5;
 
 const loader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
-loader.setDRACOLoader( dracoLoader );
+dracoLoader.setDecoderPath('/examples/jsm/libs/draco/');
+loader.setDRACOLoader(dracoLoader);
 
 const handModels = {
 	left: null,
@@ -73,7 +75,7 @@ animate();
  * Initializes the scene, camera, renderer, lighting, and AR functionalities.
  */
 function init() {
-	
+
 	scene = new Scene();
 	// setupGroups()
 	setupCamera();
@@ -86,24 +88,28 @@ function init() {
 
 }
 
-let i = 0; 
-   
+let questionIndex = 0;
+
 // Set up the anchor
 function setupAnchor(anchor) {
 
 	console.log("anchor is being loaded...")
 
+	const group = new Group();
 	anchorModel = new Group();
 	anchorText = new Text();
 
-	anchor.add(anchorText)
-	anchor.add(anchorModel)
-	
-	const questions = fetch('/question.json')
+	group.add(anchorText);
+	group.add(anchorModel);
+	group.position.set(0, 0, -2);
+	scene.add(group);
+
+	fetch('/question.json')
 		.then(response => response.json())
 		.then(data => {
-			setupGroups(anchor)
-			// setupQuestion(data[i])
+			// setupGroups(anchor)
+			questions = data
+			setupQuestion(questions[0])
 		});
 }
 
@@ -126,28 +132,54 @@ function setupCamera() {
 function setupQuestion(data) {
 	// Read the Data
 	// Replace the answer in the question with _
-	const question = data.word.replace(data.answer, "_")
 
-	// Load the model
-	anchorText.text = question
 	anchorText.frustumCulled = false	// always render
+	anchorText.anchorX = 'center';
+	anchorText.anchorY = 'bottom';
 	anchorText.fontSize = 1
-	anchorText.position.set(0, 1, 0)
-	anchorText.sync()
 
 	const loader = new GLTFLoader();
+	console.log("loading...", data.model)
+	if(anchorModel.children.length > 0) {
+		anchorModel.remove(anchorModel.children[0])
+		anchorText.text = "Loading..."
+		anchorText.sync()
+	}
 	loader.load(data.model, function (gltf) {
+		const question = data.word.replace(data.answer, "_")
+		correctAnswer = data.answer
+		anchorText.text = question
+		anchorText.sync();
+
+		// remove the model inside anchorModel 
 		const model = gltf.scene;
-		model.scale.set(data.scale, data.scale, data.scale)
+		model.scale.set(1, 1, 1)
 		model.frustumCulled = false	// always render
-		model.position.set(0, 0, -2)
+		model.position.set(0, 0, 0)
+
+		const aabb = new Box3();
+		aabb.setFromObject(model);
+
+		// from the box3, get the height of the model and scale it to 1
+		console.log(aabb.max.y)
+		const scale = 0.60 / aabb.max.y
+		console.log(scale)
+		model.scale.set(scale, scale, scale)
+
+		const height = (aabb.max.y - aabb.min.y) * scale / 2;
+		model.position.y = height;
+
+		anchorText.position.set(0, (aabb.max.y - aabb.min.y) * scale, -1)
+
 		anchorModel.add(model);
+
+		listenPose = true
 	});
 
-	console.log("anchor is loaded!")
-	console.log(anchorModel)
-	console.log(anchorText)
-
+	// console.log("anchor is loaded!")
+	// console.log(anchorModel)
+	// console.log(anchorText)
+	// TODO:
 	// Animate the model (transition in)
 	// Animate the model (transition out)
 }
@@ -243,7 +275,7 @@ function setupController() {
 	// console.log(hand1);
 
 	hand1.addEventListener('pinchend', function () {
-		if(anchorCreated == false) {
+		if (anchorCreated == false) {
 			pendingAnchorData = {
 				position: hand1.position.clone(),
 				quaternion: hand1.quaternion.clone(),
@@ -275,7 +307,7 @@ function setupController() {
 	}
 
 	hand2.addEventListener('pinchend', function () {
-		if(anchorCreated == false) {
+		if (anchorCreated == false) {
 			pendingAnchorData = {
 				position: hand1.position.clone(),
 				quaternion: hand1.quaternion.clone(),
@@ -289,7 +321,7 @@ function setupController() {
 
 	if (handyLeft == null) {
 		handyLeft = Handy.hands.getLeft()
-		if(handyLeft) {
+		if (handyLeft) {
 			console.log("found left!")
 			handyLeft.addEventListener("pose changed", (event) => {
 				// console.log(event.message)
@@ -298,7 +330,7 @@ function setupController() {
 	}
 	if (handyRight) {
 		handyRight = Handy.hands.getRight()
-		if(handyRight) {	
+		if (handyRight) {
 			console.log("found right!")
 			handyRight.addEventListener("pose changed", (event) => {
 				// console.log(event.message)
@@ -389,6 +421,8 @@ function setupGroups(anchor) {
 	text1 = new Text()
 	text1.text = 'L_MP'
 	// text1.position.set(-2, 2, -2)
+	text1.anchorX = 'center';
+	text1.anchorY = 'bottom';
 	text1.frustumCulled = false	// always render
 	text1.fontSize = 1
 
@@ -401,17 +435,16 @@ function setupGroups(anchor) {
 		model.scale.set(1, 1, 1)
 		model.frustumCulled = false	// always render
 		model.position.set(0, 0, -2)
-
 		const aabb = new Box3();
-		aabb.setFromObject( model );
-
+		aabb.setFromObject(model);
 		const height = (aabb.max.y - aabb.min.y) / 2;
 		model.position.y = height;
-		scene.add(model);
+		group1.add(model);
 
 		text1.position.set(0, (aabb.max.y - aabb.min.y), -2)
 	});
-	group1.position.set(anchor.position.x,  anchor.position.y,  anchor.position.z);
+	// TODO: should be directly infront of user
+	group1.position.set(0, 0, -2);
 	scene.add(group1)
 
 	// start listening for the pose
@@ -500,7 +533,7 @@ function render(arg) {
 		// 		// console.log(event.message)
 		// 	})
 		// }
-	} else if(listenPose) {
+	} else if (listenPose) {
 		// if(handyLeft.isPose('asl a', 3000)) {
 		// 	handyLeft.traverse((child) => { if(child.material) child.material.color = new Color( "green" ) })
 		// } else {
@@ -511,35 +544,39 @@ function render(arg) {
 	// RIGHT HAND
 	if (handyRight == null) {
 		handyRight = Handy.hands.getRight()
-		if(handyRight) {
+		if (handyRight) {
 			console.log("right hand event registered")
 			handyRight.addEventListener("pose changed", (event) => {
-				// console.log(event.message)
+				console.log(event.message)
 			})
 		}
-	} else if(listenPose) {
-		if(hand2.isPose('asl a', 3000)) {
+	} else if (listenPose) {
+		if (hand2.isPose(`asl ${correctAnswer?.toLowerCase()}`, 3000)) {
 			// Get the time away from last frame in threejs
 			userCorrectAnswerInterval += lastTime
-			if(userCorrectAnswerInterval > correctIntervalThreshold) {
+			if (userCorrectAnswerInterval > correctIntervalThreshold) {
 				// user has held the pose for 3 seconds
 				// transition to the next state
 				// Play success audio 
+				listenPose = false
 				userCorrectAnswerInterval = correctIntervalThreshold;
-				text1.text = 'LAMP'
-				text1.sync()
+				anchorText.text = anchorText.text.replace("_", correctAnswer)
+				anchorText.sync()
 				// next question
+				setTimeout(() => {
+					questionIndex += 1
+					setupQuestion(questions[questionIndex])
+				}, 3000);
 			}
-
 			// if time reached 3 seconds, then user has held the pose for 3 seconds
 		} else {
-			if(userCorrectAnswerInterval > 0) {
+			if (userCorrectAnswerInterval > 0) {
 				const newInterval = userCorrectAnswerInterval - (lastTime * incorrectAnswerMultipler)
-				userCorrectAnswerInterval = newInterval > 0 ? newInterval : 0	
+				userCorrectAnswerInterval = newInterval > 0 ? newInterval : 0
 			}
 		}
 		// console.log(userCorrectAnswerInterval)
-		handyRight.traverse((child) => { if(child.material) child.material.color = new Color().lerpColors(defaultColor, successColor, userCorrectAnswerInterval / 3000) })
+		handyRight.traverse((child) => { if (child.material) child.material.color = new Color().lerpColors(defaultColor, successColor, userCorrectAnswerInterval / 3000) })
 	}
 	Handy.update()
 	renderer.render(scene, camera);
